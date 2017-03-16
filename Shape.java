1e52da440c2431b795203285e71ccd0587f99ac7
Aayush Shah <aayushshah823@users.noreply.github.com>
2017-03-15 17:00:54 -0700

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -3,55 +3,74 @@ import java.awt.*;
  * This is an interface class
  * @author Aayush Shah
  */
-public interface Shape{
-  /**
-   * Checking if point is on shape or not
-   * @param x -- The X coordinate
-   * @param y -- The Y Coordinate
-   * @return true is yes, false otherwise
-   */ 
-  public boolean isOn(int x, int y);
-  
-  /** 
-   * Boolean method check if selected
-   * @return true if selected, false otherwise
-   */ 
-  public boolean isSelected();
-  
-  /**
-   * get method for color
-   * @return the color of shape
-   */
-  public Color getColor();
-  
-  /**
-   * Set method for color
-   * @param c The color
-   */
-  public void setColor(Color c);
-  /**
-   * Setter method 
-   * @param b The boolean value
-   */
-  public void setSelected(boolean b); 
-  
-  /**
-   * Shifting by x pixels
-   * @param deltaX The x Coodrinate for shifting
-   * @param deltaY The y coordinate for shifting
-   */ 
-  public void shiftUpperLeftBy(int deltaX, int deltaY);
-  
-  /**
-   * moving by x pixels
-   * @param newX The x Coodrinate for moving
-   * @param newY The y coordinate for moving
-   */
-  public void moveUpperLeftTo(int newX, int newY);  
-  /**
-   * ToString method
-   * @return string representation
-   */ 
-  public String toString();
-  
+public interface Shape {
+	/**
+	 * Determines whether point (x, y) is on the Shape
+	 * @param x x-coordinate of point
+	 * @param y y-coordinate of point
+	 * @return true if the point (x,y) is on the Shape, false if not
+	 */
+	public boolean isOn(int x, int y);
+	
+	/**
+	 * Determines whether or not the Shape is selected
+	 * @return true if this Shape is selected, false if not
+	 */
+	public boolean isSelected();
+	
+	/**
+	 * Selects or de-selects Shape
+	 * @param sel tells whether to select or deselect Shape
+	 */
+	public void setSelected(boolean sel);
+	
+	/**
+	 * Determines color of Shape
+	 * @return color of Shape
+	 */
+	public Color getColor();
+	
+	/**
+	 * Changes color of Shape
+	 * @param color what to set this.color to
+	 */
+	public void setColor(Color color);
+	
+	/**
+	 *
+	 * @param deltaX units to shift shape by on x-axis
+	 * @param deltaY units to shift shape by on y-axis
+	 */
+	public void shiftUpperLeftBy(int deltaX, int deltaY);
+	
+	/**
+	 * 
+	 * @param newX what to set the x-coordinate to
+	 * @param newY what to set the y-coordinate to
+	 */
+	public void moveUpperLeftTo(int newX, int newY);
+	
+	/**
+	 * Returns upper-left corner point of the Shape
+	 * @return upper-left corner point
+	 */
+	public Point getUpperLeft();
+	
+	/**
+	 * Draws the Shape
+	 * @param g Graphics to draw shape in
+	 */
+	public void drawShape(Graphics g);
+	
+	/**
+	 * Draws outline of Shape (only occurs if Shape selected)
+	 * @param g
+	 */
+	public void drawBorder(Graphics g);
+	
+	/**
+	 * Provides a String representation of Shape's state
+	 * @return String representation of Shape's state
+	 */
+	public String toString();
 }
